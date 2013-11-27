41ef4c18121fd88ba3beef547bd02859eb2e5eb7
Henning Kopp <kopp@mathematik.uni-kl.de>
2013-11-27 19:45:38 +0100

    added screenrc

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 backupdir=~/dotfiles.old  #directory for old dotfiles
 dotfiledir=~/dotfiles       #directory for new dotfiles
-dotfiles='bashrc vimrc vim singularrc'  #files to symlink
+dotfiles='bashrc screenrc singularrc vimrc vim'  #files to symlink
 
 mkdir "$backupdir"
 cd ~/
