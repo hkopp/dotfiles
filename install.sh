650431d0e79a62b2af48e7221d73035275fa44d8
Henning Kopp <henning.kopp@uni-ulm.de>
2014-05-07 12:22:57 +0200

    new inputrc (readlineconfig)

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 backupdir=~/dotfiles.old  #directory for old dotfiles
 dotfiledir=~/dotfiles       #directory for new dotfiles
-dotfiles='gitconfig bashrc i3 i3status.conf magmarc screenrc singularrc vimrc vim'  #files to symlink
+dotfiles='bashrc gitconfig i3 i3status.conf inputrc magmarc screenrc singularrc vimrc vim'  #files to symlink
 
 mkdir "$backupdir"
 cd ~/
